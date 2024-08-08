9cff4d563730c9ba39ee246b16b15976fd34bff7
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-08-08 17:20:56 -0500

    Add routing to writer

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-page',
@@ -8,11 +9,18 @@ import { Component } from '@angular/core';
 export class StartPageComponent {
   selectedFileName: string | null = null;
 
+  constructor(private router: Router){}
+
   onFileSelected(event: any): void {
     const file: File = event.target.files[0];
     if (file) {
       this.selectedFileName = file.name;
     }
+  }
+
+  goToWriter(){
+    console.log("Navigate to writer");
+    this.router.navigateByUrl("/writer");
   }
 
   submitFile(): void {
