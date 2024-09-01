02ec13a9fe678c53b839da7d8fe31df2e65baa38
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2024-09-01 07:09:33 -0500

    Add browser warning on exit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ChangeService } from './services/change-service/change.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inkwell';
+
+  constructor(private change:ChangeService){}
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any): void {
+    if (this.change.getChangeStatus()) {
+      $event.returnValue = 'You have unsaved changes! Are you sure you want to leave?';
+    }
+  }
 }
